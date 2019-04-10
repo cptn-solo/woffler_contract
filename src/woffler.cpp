6bce3358ced6c2c282b8223b2eabd96d16215dbe
Konstantin Krupovich <kkrupovich@gmail.com>
2019-04-10 14:48:52 +0300

    #5 Registration: added check for existance in players table

--- src/woffler.cpp
+++ src/woffler.cpp
@@ -3,8 +3,15 @@
 ACTION woffler::signup(name account) {
   require_auth(account);
   print("Register user: ", name{account});
-  _players.emplace(get_self(), [&](auto& p) {
+
+  auto itr = _players.find(account.value);
+  if (itr == _players.end()) {
+      _players.emplace(get_self(), [&](auto& p) {
         p.account = account;
         p.levelresult = playerstate::INIT;
-    });
+      });
+    print(", added");
+  } else {
+    print(", exists");
+  }
 }
