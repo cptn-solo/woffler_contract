3d2199f92f8eb12d8f0c007953119cc5d7a64a00
Konstantin Krupovich <kkrupovich@gmail.com>
2019-04-16 15:30:10 +0300

    #12 Sales channel table: added table and implemented channel create/increment logic upon account registration; #2 IA models: updated according to channel logic modifications;

--- include/woffler.hpp
+++ include/woffler.hpp
@@ -11,8 +11,16 @@ CONTRACT woffler : public contract {
     using contract::contract;
     woffler(name receiver, name code, datastream<const char*> ds): 
       contract(receiver, code, ds) {}
+    
+    //** Contract: **//
+    
+    //signup new player with custom sales channel (via referral link)
+    ACTION signup(name account, name channel);
+    
+    //forget player (without balance check yet, TBD!)
+    ACTION forget(name account);
 
-    ACTION signup(name account, uint64_t idchannel);
+    //withdraw player's funds to arbitrary account (need auth by player)
     ACTION withdraw (name from, name to, asset amount, const string& memo);
 
     [[eosio::on_notify("eosio.token::transfer")]]
@@ -20,14 +28,21 @@ CONTRACT woffler : public contract {
         
     using transferAction = action_wrapper<"transfer"_n, &woffler::transferHandler>;
 
+    //** Sales channels: **//
+
+    //merge channel balance into owner's active balance
+    ACTION chnmergebal(name owner);
+
   private:
     bool addBalance(name to, asset amount);
     void subBalance(name from, asset amount);
+    bool clearAccount(name account, name scope);
 
+    //players with there balances and in-game state
     TABLE wflplayer {
       name account;
+      name channel;
       uint64_t idlvl;
-      uint64_t idchannel;
       asset activebalance;
       asset vestingbalance;
       uint8_t tryposition;
@@ -38,5 +53,16 @@ CONTRACT woffler : public contract {
       
       uint64_t primary_key() const { return account.value; }
     };
-    typedef multi_index<"players"_n, wflplayer> playerstable;        
+    typedef multi_index<"players"_n, wflplayer> players;        
+    
+    //sales channels with user counter and current revenue balance available to merge into channel owner's balance
+    TABLE wflchannel {
+      name owner;
+      uint64_t height;
+      asset balance;
+      
+      uint64_t primary_key() const { return owner.value; }
+    };
+    typedef multi_index<"channels"_n, wflchannel> channels;        
+
 };
--- src/woffler.cpp
+++ src/woffler.cpp
@@ -1,12 +1,17 @@
 #include <woffler.hpp>
 
-void woffler::signup(name account, uint64_t idchannel) {
+//*** Contract scope methods ***//
+
+//register new player and assing to (existing) sales channel or contract channel
+void woffler::signup(name account, name channel) {
   require_auth(account);
   print("Register user: ", name{account});
 
   auto self = get_self();
 
-  playerstable _players(self, self.value);
+  auto _channel = (channel ? channel : self);
+  
+  players _players(self, self.value);
   
   auto player = _players.find(account.value);
   
@@ -14,15 +19,70 @@ void woffler::signup(name account, uint64_t idchannel) {
     player == _players.end(), 
     "Account already exists"
   );
+
+  //validating sales channel
+  if (_channel != self) { //channel account must be signed up already
+    players _chnlaccts(self, self.value);
+    
+    auto chnlacct = _chnlaccts.find(channel.value);
+
+    check(
+      chnlacct != _players.end(),
+      "Channel account does not exits"
+    );
+  }
   
+  //creating player record
   _players.emplace(self, [&](auto& p) {
     p.account = account;
     p.levelresult = playerstate::INIT;
     //TODO: check existence of the channel and use 0 if no channel found
-    p.idchannel = idchannel;
+    p.channel = _channel;
     p.activebalance = asset{0, acceptedSymbol};
     p.vestingbalance = asset{0, acceptedSymbol};
   });
+  
+  //creating/incrementing sales channel
+  channels _channels(self, self.value);
+  auto achannel = _channels.find(_channel.value);
+
+  if (achannel == _channels.end()) {
+    _channels.emplace(self, [&](auto& c) {
+      c.owner = _channel;
+      c.height = 1;
+      c.balance = asset{0, acceptedSymbol};
+    });
+  } 
+  else {
+    _channels.modify(achannel, self, [&]( auto& p ) {
+      p.height++;     
+    });
+  }
+}
+// remove account from players table from contract (and eosio.token, legacy) scope
+void woffler::forget(name account) {
+  require_auth(account);
+  print("Forget user: ", name{account});
+
+  clearAccount(account, get_self());
+  clearAccount(account, name("eosio.token"));//legacy
+}
+
+bool woffler::clearAccount(name account, name scope) {
+  auto self = get_self();
+
+  players _players(self, scope.value);
+  
+  auto player = _players.find(account.value);
+  
+  if (player == _players.end()) 
+    return false;
+
+  _players.erase(player);
+  
+  print("Removed user: ", name{account}, " from scope: ", name{});
+  
+  return true;
 }
 
 void woffler::transferHandler(name from, name to, asset amount, string memo) {
@@ -33,11 +93,13 @@ void woffler::transferHandler(name from, name to, asset amount, string memo) {
     "Only " + acceptedCurr.to_string() + " transfers allowed"
   );
 
-  if (to == get_self()) { //deposit
+  auto self = get_self();
+
+  if (to == self) { //deposit
     bool deposited = woffler::addBalance(from, amount);
 
     if (!deposited) {
-      signup(from, 0);
+      signup(from, self);
       addBalance(from, amount);
     }
   } 
@@ -61,7 +123,7 @@ void woffler::withdraw (name from, name to, asset amount, const string& memo) {
 bool woffler::addBalance(name to, asset amount) {
   auto self = get_self();
 
-  playerstable _players(self, self.value);
+  players _players(self, self.value);
   
   const auto& player = _players.find(to.value);
   
@@ -80,7 +142,7 @@ bool woffler::addBalance(name to, asset amount) {
 void woffler::subBalance(name from, asset amount) {
   auto self = get_self();
 
-  playerstable _players(self, self.value);
+  players _players(self, self.value);
   
   const auto& player = _players.find(from.value);
   
@@ -97,4 +159,10 @@ void woffler::subBalance(name from, asset amount) {
   _players.modify(player, self, [&]( auto& p ) {
     p.activebalance -= amount;     
   });     
+}
+
+//*** Sales channel scope methods (wflChannel) ***//
+
+void woffler::chnmergebal(name owner) {
+
 }
