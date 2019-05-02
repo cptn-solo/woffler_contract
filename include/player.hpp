913bdd95245dc969a92ff6912e3507bce097efa9
Konstantin Krupovich <kkrupovich@gmail.com>
2019-05-02 18:52:47 +0300

    #42 Try to implement cursor-like reference for an object representing class data (wflPlayer/wflLevel/wflBranch, etc.) - now no compile warnings and player class is ready to be extended or ported back to the original contract structure;

--- include/player.hpp
+++ include/player.hpp
@@ -6,41 +6,62 @@ namespace Woffler {
     using std::string;
     
     namespace Player {
+        //players with there balances and in-game state
+        typedef struct
+        [[eosio::table("players"), eosio::contract("woffler")]]
+        wflplayer {
+            name account;
+            name channel;
+            uint64_t idlvl = 0;
+            asset activebalance = asset{0, Const::acceptedSymbol};
+            asset vestingbalance = asset{0, Const::acceptedSymbol};
+            uint8_t tryposition = 0;
+            uint8_t currentposition = 0;
+            uint8_t triesleft = 0;
+            uint8_t levelresult = Const::playerstate::INIT;
+            uint32_t resulttimestamp;
+            
+            uint64_t primary_key() const { return account.value; }
+        } wflplayer;
+        
+        typedef multi_index<"players"_n, wflplayer> players;    
+
+        
         class Player {
             public:
-            Player(name self, name account);
-            //players with there balances and in-game state
-            typedef struct
-            [[eosio::table("players"), eosio::contract("woffler")]]
-            wflplayer {
-                name account;
-                name channel;
-                uint64_t idlvl = 0;
-                asset activebalance = asset{0, Const::acceptedSymbol};
-                asset vestingbalance = asset{0, Const::acceptedSymbol};
-                uint8_t tryposition = 0;
-                uint8_t currentposition = 0;
-                uint8_t triesleft = 0;
-                uint8_t levelresult = Const::playerstate::INIT;
-                uint32_t resulttimestamp;
-                
-                uint64_t primary_key() const { return account.value; }
-            } wflplayer;
-            typedef multi_index<"players"_n, wflplayer> players;    
 
-            const wflplayer& getPlayer();
-            
-            void checkNoPlayer();
-            
-            void createPlayer(name payer, name channel);            
+                Player(name self, name account);
+
+                void createPlayer(name payer, name channel);            
 
             private:
-            name _self;
-            name _player;
-            players::const_iterator _pitr;
-            
-            players _players;     
+
+                name _self;
+                name _player;            
+                players _players;     
 
         };
+
+        struct DAO {
+            public:
+
+                DAO(players& players, name player);      
+
+                template<typename Lambda>
+                void create(name payer, Lambda&& creator);
+
+                template<typename Lambda>
+                void update(name payer, Lambda&& updater);
+                
+                const wflplayer& getPlayer();
+                void checkNoPlayer();
+                void create(name payer, name player, name channel);
+            
+            private:
+
+                players& _players;     
+                players::const_iterator _pitr;
+        };
+
     }
 }
--- src/player.cpp
+++ src/player.cpp
@@ -4,44 +4,54 @@
 
 namespace Woffler {
     namespace Player {
-        Player::Player(name self, name account): 
-            _players(self, self.value),
-            _pitr(_players.end())
-        {
+        Player::Player(name self, name account) : _players(self, self.value) {
             this->_self = self;
             this->_player = account;
-            this->_pitr = _players.find(account.value);
-        }
+            //this->_pitr = _players.find(account.value);
+        }        
         
-        /*** 
-          usage (check for existance before use!!!):
-          auto p = getPlayer();
-          print("player: ", name{p.account});
-        */
-        const Player::wflplayer& Player::getPlayer() {
-            return *_pitr;
-        }
-        
-        void Player::checkNoPlayer() {      
-            check(
-                _pitr == _players.end(), 
-                "Account already exists"
-            );
-        }
-
         void Player::createPlayer(name payer, name channel) {
-            auto _channel = (channel ? channel : _self);
             check(
                 _player == _self || channel != _player, //only contract account can be register by his own
                 "One can not be a sales channel for himself"
             );
 
-            checkNoPlayer();
+            auto _channel = (channel ? channel : _self);
 
-            _players.emplace(payer, [&](auto& p) {
+            DAO _dao(_players, _player);
+            _dao.checkNoPlayer();
+            _dao.create(payer, [&](auto& p) {
                 p.account = _player;
                 p.channel = _channel;
             });
+        }
+
+        DAO::DAO(players& players, name player): _pitr(players.find(player.value)), _players{players} {}
+        
+        template<typename Lambda>
+        void DAO::create(name payer, Lambda&& creator) {
+            _pitr = _players.emplace(payer, std::forward<Lambda&&>(creator)); 
+        }
+
+        template<typename Lambda>
+        void DAO::update(name payer, Lambda&& updater) {
+            _players.modify(_pitr, payer, std::forward<Lambda&&>(updater)); 
+        }
+
+        /*** 
+          usage (check for existance before use!!!):
+          auto p = getPlayer();
+          print("player: ", name{p.account});
+        */
+        const wflplayer& DAO::getPlayer() {
+            return *_pitr;
+        }
+            
+        void DAO::checkNoPlayer() {      
+            check(
+                _pitr == _players.end(), 
+                "Account already exists"
+            );
         }
     }
     
