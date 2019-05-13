20e6d11ceec390165b5d9dbc379eadb20c0f42df
Konstantin Krupovich <kkrupovich@gmail.com>
2019-05-13 19:54:14 +0300

    #46 Level generation algorithm optimisation: just some cleanup;

--- include/utils.hpp
+++ include/utils.hpp
@@ -20,27 +20,8 @@ namespace Utils {
     return time_point_sec(current_time_point()).utc_seconds;
   }
 
-  template<typename T>
-  void printVectorInt(const std::vector<T>& data) {
-    for (typename std::vector<T>::const_iterator i = data.begin(); i != data.end(); ++i)
-      print("[", std::to_string(*i), "]");
-  }
   inline uint128_t combineIds(const uint64_t& x, const uint64_t& y) {
     return (uint128_t{x} << 64) | y;
-  }
-
-  template<typename T>
-  bool hasIntersection(std::vector<T>& v1, std::vector<T>& v2) {
-    typename std::vector<T> v3;
-    
-    //our vectors are sorted by design
-    // std::sort(v1.begin(), v1.end());
-    // std::sort(v2.begin(), v2.end());
-
-    std::set_intersection(v1.begin(), v1.end(),
-                          v2.begin(), v2.end(),
-                          back_inserter(v3));
-    return !v3.empty();
   }
 
   bool hasIntersection(uint16_t& v1, uint16_t& v2) {    
